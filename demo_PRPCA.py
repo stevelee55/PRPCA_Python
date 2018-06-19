6da7e31859b41c0a1ddf0095558f8b4d57f48d9b
stevesl <stevesl@umich.edu>
2018-06-19 16:56:20 -0400

    Script can now add specific files to the matrix

--- demo_PRPCA.py
+++ demo_PRPCA.py
@@ -30,48 +30,46 @@ from skimage import transform
 # Gets the list of names of the frames along with the
 # number of total frames.
 contents = os.listdir("./Data/tennis")
-numberOfElementsInContents = len(contents)
-img = imread("Data/tennis/00000.jpg")
-# Getting the new width and height for the video frame.
-newWidth = len(img) * 0.5
-newHeight = len(img[0]) * 0.5
-img = transform.resize(img, (newWidth, newHeight))
+contents.sort()
+# Getting only the ones that have the certain extension.
+# In this case, only .jpg
+videoFrames = []
+for content in contents:
+	if content.endswith('.jpg'):
+		videoFrames.append(content)
 
-# These are used to present and show the imported image.
+numberOfVideoFrames = len(videoFrames)
+
+# Getting the new width and height for the video frame.
+img = imread("Data/tennis/00000.jpg")
 # print(img)
-# plt.imshow(img)
-# plt.show()
+# print(img[0][0][0])
+width = len(img)
+height = len(img[0])
+newWidth = width * 0.5
+newHeight = height * 0.5
 
 # Initializing the 4-D Matrix with 0 values.
 # Apparently the matrix is width * height * 3 * # of frames of the video.
-numberOfVideoFrames = numberOfElementsInContents
-width = 20
-height = 5
 mys = 3
 MovMat = [[[[0 for x in range(width)] for y in range(height)] for m in range(mys)] for z in range(numberOfVideoFrames)]
 
-	#Checking to see if the MovMat is initizlied correctly.
-counter = 0
-
-	# for z in range(numberOfVideoFrames):
-	# 	for y in range(height):
-	# 		for x in range(width):
-	# 			counter+=1
-
-	# print(counter)
-
 # This is 0-Indexed Matrix. Goes through every single
 # frame and sets it to the MovMat.
-for a in range(numberOfElementsInContents):
+for i in range(numberOfVideoFrames):
 	# Getting the specific frame and saving in the 4-D array.
-	#print(MovMat[2][0][0])
-	counter+=1
+	img = imread("./Data/tennis/" + videoFrames[i])
+	MovMat[i] = transform.resize(img, (newWidth, newHeight))
 
 	#Description
 	#example
 	#B = imresize(A,scale) returns image B that is scale times the size of A. The input image A can be a grayscale, RGB, or binary image. If A has more than two dimensions, imresize only resizes the first two dimensions. If scale is in the range [0, 1], B is smaller than A. If scale is greater than 1, B is larger than A. By default, imresize uses bicubic interpolation.
 
 
+# These are used to present and show the imported image.
+# print(img)
+# plt.imshow(img)
+# plt.show()
 
 
 # RGB PRPCA
