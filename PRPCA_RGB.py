af87f303cbe991ce29c2ba9dd08bac40de03b6ff
stevesl <stevesl@umich.edu>
2018-06-22 14:28:55 -0400

    parseField function is done and tested (sort of)

--- PRPCA_RGB.py
+++ PRPCA_RGB.py
@@ -1,16 +1,45 @@
+import struct
+import math
+
 class PRPCA_RGB(object):
 
-
+	# Initializer for the this class' instance.
 	def __init__(self):
-		print("Created!")
+		print("PRPCA_RGB is created!")
 
-	def helloWorld(self):
-		print("Hello! :S")
+	# Function that parses struct field
+	def parseField(self, stats, field, default):
+		# Value to return from the function.
+		val = None
+		# Checking if the field/key exists in the "stats" struct.
+		# "hasattr" already exists in python.
+		if hasattr(stats, field):
+			val = getattr(stats, field)
+		else:
+			val = default
+		return val
 
-	def parseInputs(self, Y, opts):
-		print("shoot")
-		print(Y)
-		print(opts)
+	# Functions for parsing inputs.
+	# 'opts' are the optional data that may or may not exist.
+	def parseInputs(self, Y): # (self, Y, opts):
+		# Below is the psudo code for the matlab code that I don't think
+		# it's necessary. For now, create the opts object in this function
+		# and use it and return it instead.
+		# if opts and var do not exist
+		# 	then opts = struct()
+		opts = struct
+
+		# Getting the calcualted LamS and nIters and returning them.
+		# the part "1 / sqrt(max(size(Y))" is the one that I could pass in infinity I think. Check on this.
+		# "max" is already built in python.
+		# Parameter for the LamS: Not sure what this really represents yet.
+		lamSParameter = 1 / math.sqrt(max(size(Y)))
+		LamS = parseField(opts, "LamS", lamSParameter)
+		nIters = parseField(opts, "nIters", 100)
+
+	def parseInputs2(self, opts):
+		return "Defautl"
+		
 		
 	def PRPCA_RGB_Woah(movmat):
 
